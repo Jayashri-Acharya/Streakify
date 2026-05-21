3a5ad3368b4131a52684a636bf28374f3e27a21d
Jayashri-Acharya <jayashriacharya2005@gmail.com>
2026-05-21 11:36:03 +0530

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,8 +1,26 @@
-function getData() {
-return JSON.parse(localStorage.getItem('routineData')) || defaultData;
+function cloneData(data) {
+  return typeof structuredClone === "function"
+    ? structuredClone(data)
+    : JSON.parse(JSON.stringify(data));
 }
 
+function getData() {
+  const stored = JSON.parse(localStorage.getItem("routineData"));
+
+  if (!stored) {
+    const initialData = cloneData(defaultData);
+    localStorage.setItem("routineData", JSON.stringify(initialData));
+    return initialData;
+  }
+
+  return {
+    ...cloneData(defaultData),
+    ...stored,
+    routines: stored.routines || cloneData(defaultData.routines),
+    completed: stored.completed || {}
+  };
+}
 
 function saveData(data) {
-localStorage.setItem('routineData', JSON.stringify(data));
+  localStorage.setItem("routineData", JSON.stringify(data));
 }
